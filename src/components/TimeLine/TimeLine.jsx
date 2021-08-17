5978eb06780933ac306f8b4caa59f842da22aea4
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2021-08-17 11:41:40 +0400

    corrected a typo

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -10,7 +10,7 @@ const Timeline = () => {
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
         I'm a software engineer with 2 years of experience, currently specializing in web development. I work a lot with HTML, CSS, Bootstrap,Javascript and React to build web applications.<br />
-        Currently, I am a part of a team of two as a front-end where I focus on building the client side of the application. To know more please download my CV.
+        Currently, I am a part of a team of two as a front-end developer where I focus on building the client side of the application. To know more please download my CV.
       </SectionText>
       <Button download href="/CV/abdulLatifSadiq.pdf" target="_blank" rel="noreferrer">
         Download CV</Button>
