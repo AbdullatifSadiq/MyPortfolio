a4a7a90ed44d7c6a7a87f464041e6d1ca4def6ab
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-29 07:41:52 -0400

    added a new project

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,7 +1,8 @@
 export const projects = [
   {
     title: 'Express Passage',
-    description: "Using HTML, CSS and Bootstrap I created the front-end part of a website for a freight company to help their customers track their package after it has been shipped.",
+    description:
+      'Using HTML, CSS and Bootstrap I created the front-end part of a website for a freight company to help their customers track their package after it has been shipped.',
     image: '/images/1.png',
     tags: ['HTML', 'CSS', 'Bootstrap'],
     source: 'https://expresspassage.com',
@@ -10,19 +11,30 @@ export const projects = [
   },
   {
     title: 'RealEstimate',
-    description: "Using HTML, CSS, Bootstrap and JavaScript I created the client-side of a machine learning house price prediction system.",
+    description:
+      'Using HTML, CSS, Bootstrap and JavaScript I created the client-side of a machine learning house price prediction system.',
     image: '/images/2.png',
     tags: ['HTML', 'CSS', 'Bootstrap', 'JavaScript'],
     source: 'https://github.com/AbdullatifSadiq/RealEstimate',
     visit: 'https://github.com/AbdullatifSadiq/RealEstimate',
     id: 1,
   },
+  {
+    title: 'Zillow Clone',
+    description:
+      'This is a clone for the official zillow website created using, React and regular css. The site is still under development and is only fully functional in the mobile view ',
+    image: '/images/2.png',
+    tags: ['HTML', 'CSS', 'Bootstrap', 'JavaScript'],
+    source: 'https://github.com/AbdullatifSadiq/zillowClone-react',
+    visit: 'https://zillowclone.herokuapp.com',
+    id: 2,
+  },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: 'Started my journey' },
+  { year: 2018, text: 'Worked as a freelance developer' },
+  { year: 2019, text: 'Founded JavaScript Mastery' },
+  { year: 2020, text: 'Shared my projects with the world' },
+  { year: 2021, text: 'Started my own platform' },
 ];
