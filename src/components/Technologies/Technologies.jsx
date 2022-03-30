2fde574309a733431e43130ceda583094e393ed9
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-30 06:51:58 -0400

    technologies edit

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,47 +1,59 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from './TechnologiesStyles';
 
 const Technologies = () => (
-  <Section id="tech">
+  <Section id='tech'>
     <SectionDivider />
     <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with a range of technologies in the web development world.
+      I've worked with a range of technologies in the web development ecosystem
       from front-end to design.
     </SectionText>
     <List>
-      <ListItem >
-        <DiReact size="3rem" />
+      <ListItem>
+        <DiReact size='3rem' />
         <ListContainer>
           <ListTitle>Front-end</ListTitle>
           <ListParagraph>
             Experience with <br />
-            React.js <br />
+            React Js <br />
             HTML & CSS
           </ListParagraph>
         </ListContainer>
       </ListItem>
-      <ListItem >
-        <DiZend size="3rem" />
+      <ListItem>
+        <DiZend size='3rem' />
         <ListContainer>
           <ListTitle>Back-end</ListTitle>
           <ListParagraph>
             Experience with <br />
-            MongoDB<br />
-            Node
+            MongoDB
+            <br />
+            Express Js
           </ListParagraph>
         </ListContainer>
       </ListItem>
-      <ListItem >
-        <DiFirebase size="3rem" />
+      <ListItem>
+        <DiFirebase size='3rem' />
         <ListContainer>
           <ListTitle>UI/UX</ListTitle>
           <ListParagraph>
             Experience with <br />
-            Abode XD
+            Figma
           </ListParagraph>
         </ListContainer>
       </ListItem>
