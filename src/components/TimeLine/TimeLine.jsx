79ab8f3b5d2ab0f602b9c9b16814c17d140578d8
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-30 07:09:55 -0400

    about edit

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -1,19 +1,33 @@
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import { Button } from './TimeLineStyles';
 
 const Timeline = () => {
-
   return (
-    <Section id="about">
+    <Section id='about'>
       <SectionDivider />
       <br />
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        I'm a software engineer with 2 years of experience, currently specializing in web development. I work a lot with HTML, CSS, Bootstrap,Javascript and React to build web applications.<br />
-        Currently, I am a part of a team of two as a front-end developer where I focus on building the client side of the application. To know more please download my CV.
+        I'm a software engineer with 3 years of experience with solid computer
+        science background. I'm currently specializing in front-end web
+        development where I focus on building the fast and responsive web
+        application.
+        {/* I use HTML, CSS, Tailwind ,Javascript, Typescript and React to build fast and responsive web applications.<br />  */}
+        To know more please download my CV here.
       </SectionText>
-      <Button download href="/CV/abdulLatifSadiq.pdf" target="_blank" rel="noreferrer">
-        Download CV</Button>
+      <Button
+        download
+        href='/CV/abdulLatifSadiq.pdf'
+        target='_blank'
+        rel='noreferrer'
+      >
+        Download CV
+      </Button>
     </Section>
   );
 };
