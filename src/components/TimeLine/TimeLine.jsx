9a081be5a94ecc30752fe60ee352f950a4a8fb02
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-30 07:11:29 -0400

    about edit

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -17,7 +17,8 @@ const Timeline = () => {
         science background. I'm currently specializing in front-end web
         development where I focus on building the fast and responsive web
         application.
-        {/* I use HTML, CSS, Tailwind ,Javascript, Typescript and React to build fast and responsive web applications.<br />  */}
+        {/* I use HTML, CSS, Tailwind ,Javascript, Typescript and React to build fast and responsive web applications.<br />  */}{' '}
+        <br />
         To know more please download my CV here.
       </SectionText>
       <Button
